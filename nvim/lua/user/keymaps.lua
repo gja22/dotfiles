1c8c6a8aaf2e9fe945e71a9e7a0c6071f6992e7e
Geoff Apps <76828380+gja22@users.noreply.github.com>
2022-08-28 13:40:32 -0700

    initial nvim config

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,97 @@
+-- Shorten function name
+local keymap = vim.keymap.set
+-- Silent keymap option
+local opts = { silent = true }
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+
+-- Modes
+--   normal = "n",
+--   insert = "i",
+--   visual = "v",
+--   visual_block = "x",
+--   term = "t",
+--   command = "c",
+
+-- NORMAL MODE MAPPINGS --
+
+-- Save and quit
+keymap("n", "<leader>w", ":w<CR>", opts)
+keymap("n", "<leader>q", ":q<CR>", opts)
+
+-- Easy toggling of numbering
+keymap("n", "<leader>r", ":set relativenumber!<CR>", opts)
+keymap("n", "<leader>n", ":set number!<CR>", opts)
+
+-- Windows
+-- fast window creation
+keymap("n", "<leader><Right>", "<C-w>v", opts)
+keymap("n", "<leader><Down>", "<C-w>s", opts)
+-- close current window
+keymap("n", "<leader>c", ":close<CR>", opts)
+-- close other windows
+keymap("n", "<leader>o", ":only<CR>", opts)
+-- make all windows equal size
+keymap("n", "<leader>=", "<C-w>=", opts)
+-- increase height of window
+keymap("n", "<leader><Up>", "<C-w>5+", opts)
+-- increase width of window
+keymap("n", "<leader><Left>", "<C-w>5>", opts)
+-- use arrows for window navigation
+keymap("n", "<Left>", "<C-w>h", opts)
+keymap("n", "<Down>", "<C-w>j", opts)
+keymap("n", "<Up>", "<C-w>k", opts)
+keymap("n", "<Right>", "<C-w>l", opts)
+
+-- Navigate buffers
+--keymap("n", "<S-l>", ":bnext<CR>", opts)
+--keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Clear search highlighting
+keymap("n", "<leader><Space>", ":nohlsearch<CR>", opts)
+
+-- Close buffers
+--keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
+
+-- Better paste
+--keymap("v", "p", '"_dP', opts)
+
+-- INSERT MODE MAPPINGS --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- PLUGIN MAPPINGS --
+
+-- NvimTree
+--keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- Telescope
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+
+-- Git
+--keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
+
+-- Comment
+keymap("n", "<leader>/", "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", opts)
+keymap("x", "<leader>/", '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>')
+
+-- DAP
+--keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
+--keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", opts)
+--keymap("n", "<leader>di", "<cmd>lua require'dap'.step_into()<cr>", opts)
+--keymap("n", "<leader>do", "<cmd>lua require'dap'.step_over()<cr>", opts)
+--keymap("n", "<leader>dO", "<cmd>lua require'dap'.step_out()<cr>", opts)
+--keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
+--keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
+--keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
+--keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,63 @@
+-- nvim options
+
+
+-- Search
+vim.opt.incsearch = true
+vim.opt.ignorecase = true                       -- ignore case when searching
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+-- :set hlsearch! to toggle within nvim
+
+-- Display
+vim.opt.cursorline = true                       -- highlight the current line
+vim.opt.termguicolors = true                    -- allow 24-bit RGB colors in the TUI
+vim.opt.guicursor = "a:block"                   -- block cursor in all modes
+vim.opt.number = true                           -- set numbered lines
+vim.opt.relativenumber = false                  -- no relative numbering
+vim.opt.colorcolumn = "80"                      -- visually indicate line width
+vim.opt.signcolumn = "yes"                      -- show sign column (errors, etc.)
+vim.opt.wrap = false                            -- display lines as one long line
+vim.opt.linebreak = true                        -- split on whitespace, no broken words
+vim.opt.showmatch = true                        -- show matching brackets
+vim.opt.showmode = true                         -- show INSERT, VISUAL, etc. mode on command line
+vim.opt.showcmd = true                          -- show active command on the command line
+vim.opt.scrolloff = 3                           -- lines above/below when scrolling
+vim.opt.sidescrolloff = 3
+vim.opt.cmdheight = 2                           -- more space for displaying messages
+
+-- Backup
+vim.opt.swapfile = false                        -- no swapfile
+vim.opt.backup = false                          -- no backup file
+--vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.undofile = true                         -- enable persistent undo
+vim.opt.backupdir = "~/.vim/backup"
+vim.opt.directory = "~/.vim/swp"
+vim.opt.undodir = "~/.vim/undo"
+
+-- status line
+
+
+vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
+vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
+vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
+vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
+vim.opt.pumheight = 10                          -- pop up menu height
+vim.opt.showtabline = 0                         -- always show tabs
+vim.opt.smartindent = true                      -- make indenting smarter again
+vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
+vim.opt.splitright = true                       -- force all vertical splits to go to the right of current window
+vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.updatetime = 300                        -- faster completion (4000ms default)
+vim.opt.expandtab = true                        -- convert tabs to spaces
+vim.opt.shiftwidth = 2                          -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
+vim.opt.laststatus = 3
+vim.opt.ruler = false
+vim.opt.numberwidth = 4                         -- set number column width to 2 {default 4}
+--vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
+vim.opt.fillchars.eob=" "
+vim.opt.shortmess:append "c"
+vim.opt.whichwrap:append("<,>,[,],h,l")
+vim.opt.iskeyword:append("-")
+
