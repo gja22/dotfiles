c4a5223508a9b47bc4a8dd0eb6b7bacedd649732
Geoff Apps <76828380+gja22@users.noreply.github.com>
2022-09-07 07:25:44 -0700

    ensure all config files are called during initialization

--- nvim/lua/gja22/init.lua
+++ nvim/lua/gja22/init.lua
@@ -15,11 +15,17 @@ require("gja22.autocommands")
 -- Indent Blankline configuration
 require("gja22.indentline")
 
+-- Gitsigns configuration
+require("gja22.gitsigns")
+
+-- Illuminate configuration
+require("gja22.illuminate")
+
 -- Status line configuration
 require("gja22.lualine")
 
 -- Color Scheme configuration
--- require("gja22.colorscheme")
+require("gja22.colorscheme")
 
 -- Key mappings configuration
 require("gja22.keymaps")
@@ -27,14 +33,17 @@ require("gja22.keymaps")
 -- LSP configuration
 require("gja22.lsp")
 
--- Completion configuration
--- require("gja22.cmp")
+-- Telescope configuration
+require("gja22.telescope")
 
 -- Autopairs configuration
 require("gja22.autopairs")
 
 -- Comment configuration
--- require("gja22.comment")
+require("gja22.comment")
+
+-- Completion configuration
+-- require("gja22.cmp")
 
 -- Luasnip configuration
 -- require("gja22.luasnip")
