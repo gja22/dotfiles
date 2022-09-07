ab7ec35ece7695b6a21e78634425f9ba49a00a2a
Geoff Apps <76828380+gja22@users.noreply.github.com>
2022-09-07 06:51:43 -0700

    simplified options

--- nvim/after/plugin/indentline.lua
+++ nvim/after/plugin/indentline.lua
@@ -1,17 +1,16 @@
+-- Setup of indent-blankline plugin
+
 local status_ok, indent_blankline = pcall(require, "indent_blankline")
 if not status_ok then
   return
 end
 
-indent_blankline.setup {
-  char = "▏",
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = true,
+indent_blankline.setup({
+  -- There is a of configuration available. I'd like t add back the
+  -- highlighting of the active block, but keeping it simple for now.
+  --
+  -- This prevents highlighting blocks that are not semantic in golang,
+  -- but it includes the package as a block. Still figuring out how
+  -- to get around that.
   use_treesitter = true,
-  show_current_context = true,
-  buftype_exclude = { "terminal", "nofile" },
-  filetype_exclude = {
-    "help",
-    "packer",
-  },
-}
+})
