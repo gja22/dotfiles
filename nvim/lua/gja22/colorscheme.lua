ea1dcab5593c904e4283b5f107e817f03d9fcd7a
Geoff Apps <76828380+gja22@users.noreply.github.com>
2022-10-16 11:34:03 -0700

    changed default colorscheme

--- nvim/lua/gja22/colorscheme.lua
+++ nvim/lua/gja22/colorscheme.lua
@@ -1,11 +1,11 @@
 -- Set the color scheme
 
 -- local colorscheme = "gruvbox"
-local colorscheme = "tokyonight"
+-- local colorscheme = "tokyonight"
+local colorscheme = "tokyonight-night"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not status_ok then
-  return
+    return
 end
-
