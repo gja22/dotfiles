025fc1236edcbfd0ed99de00e65eb6f77917459a
Geoff Apps <76828380+gja22@users.noreply.github.com>
2022-12-18 10:32:10 -0800

    Added two new example configurations

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,10 +4,12 @@
 --   https://github.com/LunarVim/nvim-basic-ide
 --   https://github.com/tjdevries/config_manager
 --   https://github.com/ThePrimeagen/.dotfiles
+--   https://github.com/nvim-lua/kickstart.nvim
+--   https://github.com/VonHeikemen/lsp-zero.nvim
 --
 -- All configuration files are a mess.
 -- Mine are no different.
 -- Wander here at your own risk!
 
 -- Load my configuration
-require("gja22")
+require 'gja22'
