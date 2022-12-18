98e15ea58ce9947dfd1214278ff6b23c33b9095b
Geoff Apps <76828380+gja22@users.noreply.github.com>
2022-12-18 15:45:30 -0800

    cleaned up Harpoon code

--- nvim/lua/gja22/harpoon.lua
+++ nvim/lua/gja22/harpoon.lua
@@ -4,10 +4,13 @@ if not status_ok then
   return
 end
 
+local mark = require('harpoon.mark')
+local ui = require('harpoon.ui')
+
 -- Harpoon keymaps
-vim.keymap.set("n", "<leader>a", function() require("harpoon.mark").add_file() end, { desc = '' })
-vim.keymap.set("n", "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, { desc = '' })
-vim.keymap.set("n", "<C-h>", function() require("harpoon.ui").nav_file(1) end, { desc = '' })
-vim.keymap.set("n", "<C-j>", function() require("harpoon.ui").nav_file(2) end, { desc = '' })
-vim.keymap.set("n", "<C-k>", function() require("harpoon.ui").nav_file(3) end, { desc = '' })
-vim.keymap.set("n", "<C-l>", function() require("harpoon.ui").nav_file(4) end, { desc = '' })
+vim.keymap.set("n", "<leader>a", mark.add_file, { desc = '[A]dd Harpoon file' })
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = '[E]dit Harpoon files' })
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, { desc = 'Harpoon file 1' })
+vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end, { desc = 'Harpoon file 2' })
+vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end, { desc = 'Harpoon file 3' })
+vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end, { desc = 'Harpoon file 4' })
