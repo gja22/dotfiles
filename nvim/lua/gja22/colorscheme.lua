34553ceb93360df96b3f8dc53c0d6b9ba342af82
Geoff Apps <76828380+gja22@users.noreply.github.com>
2023-04-19 06:33:56 -0700

    Changed default color scheme to gruvbox

--- nvim/lua/gja22/colorscheme.lua
+++ nvim/lua/gja22/colorscheme.lua
@@ -1,10 +1,10 @@
 -- Set the color scheme
 
--- local color = 'gruvbox'
+local color = 'gruvbox'
 -- local color = 'tokyonight'
 -- local color = 'tokyonight-night'
 -- local color = 'NeoSolarized'
 -- local color = 'onedark'
-local color = 'rose-pine'
+-- local color = 'rose-pine'
 
 vim.cmd.colorscheme(color)
