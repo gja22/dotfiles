c1120c6daefebdf766df73dfe9ca5b566650ffd2
Geoff Apps <76828380+gja22@users.noreply.github.com>
2023-06-02 12:10:39 -0700

    Updated shortcut for editing Harpoon files

--- nvim/lua/gja22/harpoon.lua
+++ nvim/lua/gja22/harpoon.lua
@@ -9,7 +9,7 @@ local ui = require('harpoon.ui')
 
 -- Harpoon keymaps
 vim.keymap.set("n", "<leader>h", mark.add_file, { desc = '[H]arpoon add file' })
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = '[E]dit Harpoon files' })
+vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, { desc = '[E]dit Harpoon files' })
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, { desc = 'Harpoon file 1' })
 vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end, { desc = 'Harpoon file 2' })
 vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end, { desc = 'Harpoon file 3' })
