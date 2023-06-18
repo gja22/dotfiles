d79744f5ff4aa37ace102b19aafaf0a5b76ecf02
Geoff Apps <76828380+gja22@users.noreply.github.com>
2023-06-18 10:07:14 -0700

    Fixed fidget tag version at 'legacy'

--- nvim/lua/gja22/plugins.lua
+++ nvim/lua/gja22/plugins.lua
@@ -33,7 +33,10 @@ return packer.startup(function(use)
             'williamboman/mason.nvim',
             'williamboman/mason-lspconfig.nvim',
             -- Useful status updates for LSP
-            'j-hui/fidget.nvim',
+            {
+                'j-hui/fidget.nvim',
+                tag = 'legacy',
+            },
         },
     }
 
