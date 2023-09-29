89cb6ed9b20bd67f7b850f7093a5c5f55e9b0d2d
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2023-09-29 07:51:32 -0700

    Cleaned up autopairs config formatting

--- nvim/lua/gja22/autopairs.lua
+++ nvim/lua/gja22/autopairs.lua
@@ -1,17 +1,15 @@
 -- Setup nvim-autopairs
-local status_ok, npairs = pcall(require, "nvim-autopairs")
-if not status_ok then
-  return
-end
+local autopairs = require "nvim-autopairs"
 
-npairs.setup {
-  check_ts = true, -- treesitter integration
-  disable_filetype = { "TelescopePrompt" },
+autopairs.setup {
+    check_ts = true, -- treesitter integration
+    disable_filetype = { "TelescopePrompt" },
 }
 
+-- If you want insert `(` after select function or method item
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then
-  return
-end
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done {})
+local cmp = require "cmp"
+cmp.event:on(
+    "confirm_done",
+    cmp_autopairs.on_confirm_done()
+)
