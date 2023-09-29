1923ba55654792f7ae004543c8d22e2149b57270
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2023-09-29 07:52:05 -0700

    Changed default colorscheme to kanagawa

--- nvim/lua/gja22/colorscheme.lua
+++ nvim/lua/gja22/colorscheme.lua
@@ -1,10 +1,11 @@
 -- Set the color scheme
 
-local color = 'gruvbox'
+-- local color = 'gruvbox'
 -- local color = 'tokyonight'
 -- local color = 'tokyonight-night'
 -- local color = 'NeoSolarized'
 -- local color = 'onedark'
 -- local color = 'rose-pine'
+local color = 'kanagawa'
 
 vim.cmd.colorscheme(color)
