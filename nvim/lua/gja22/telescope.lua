b26ac5d5b4ba67b7c189c4ea7a1e20f0fb9acb6a
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2023-09-30 06:12:01 -0700

    Consistent use of ' and "

--- nvim/lua/gja22/telescope.lua
+++ nvim/lua/gja22/telescope.lua
@@ -1,12 +1,12 @@
 -- Telescope configuration
 
-require("telescope").setup {
+require('telescope').setup {
     defaults = {
 
         prompt_prefix = " ",
         selection_caret = " ",
-        path_display = { "truncate" },
-        file_ignore_patterns = { ".git/", "node_modules" },
+        path_display = { 'truncate' },
+        file_ignore_patterns = { '.git/', 'node_modules' },
 
         mappings = {
             i = {
@@ -15,14 +15,14 @@ require("telescope").setup {
                 -- Use `C-p` previous item in list
                 -- Use `C-n` next item in list
 
-                ["<Down>"] = require('telescope.actions').cycle_history_next,
-                ["<Up>"] = require('telescope.actions').cycle_history_prev,
+                ['<Down>'] = require('telescope.actions').cycle_history_next,
+                ['<Up>'] = require('telescope.actions').cycle_history_prev,
                 -- C-n for next is available by default
-                ["<C-j>"] = require('telescope.actions').move_selection_next,
+                ['<C-j>'] = require('telescope.actions').move_selection_next,
                 -- C-p for previous is available by default
-                ["<C-k>"] = require('telescope.actions').move_selection_previous,
-                ["<C-u>"] = false,
-                ["<C-d>"] = false,
+                ['<C-k>'] = require('telescope.actions').move_selection_previous,
+                ['<C-u>'] = false,
+                ['<C-d>'] = false,
             },
         },
     },
@@ -32,24 +32,24 @@ require("telescope").setup {
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('file_browser')
 
--- Telescope Keymaps
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+-- Telescope keymaps
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = "[?] Find recently opened files" })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = "[ ] Find existing buffers" })
 vim.keymap.set('n', '<leader>/', function()
     require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
         winblend = 10,
         previewer = false,
     })
-end, { desc = '[/] Fuzzily search in current buffer' })
+end, { desc = "[/] Fuzzily search in current buffer" })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch [W]ord under cursor' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = "[S]earch [W]ord under cursor" })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = "[S]earch [R]esume" })
 
-vim.keymap.set('n', '<leader>fb', ':Telescope file_browser<cr>', { desc = '[F]ile [B]rowser', noremap = true, })
+vim.keymap.set('n', '<leader>fb', ':Telescope file_browser<cr>', { desc = "[F]ile [B]rowser", noremap = true, })
 
-vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>gc', require('telescope.builtin').git_status, { desc = '[G]it [C]hanges' })
+vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = "Search [G]it [F]iles" })
+vim.keymap.set('n', '<leader>gc', require('telescope.builtin').git_status, { desc = "[G]it [C]hanges" })
