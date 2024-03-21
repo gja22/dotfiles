81efde43d5620a50df43ea5941899fe5619b72af
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2024-03-21 15:53:02 -0700

    Reformat and added category names.

--- nvim/lua/gja22/plugins/whichkey.lua
+++ nvim/lua/gja22/plugins/whichkey.lua
@@ -1,14 +1,22 @@
 -- https://github.com/folke/which-key.nvim
 return {
-    'folke/which-key.nvim',
-    -- event = 'VeryLazy',
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-    end,
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration at the above URL
-    }
+	"folke/which-key.nvim",
+	event = "VimEnter",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration at the above URL
+	},
+	config = function()
+		require("which-key").setup()
+		require("which-key").register({
+			["<leader>s"] = { name = "Search", _ = "which_key_ignore" },
+			["<leader>t"] = { name = "Toggle", _ = "which_key_ignore" },
+			["<leader>z"] = { name = "Zettel", _ = "which_key_ignore" },
+		})
+	end,
 }
