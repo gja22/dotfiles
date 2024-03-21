0d75a8d7248e6be2fe0829c0f323663a51fb0aaa
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2024-03-21 16:04:14 -0700

    Reformat and keymap change

--- nvim/lua/gja22/plugins/indentline.lua
+++ nvim/lua/gja22/plugins/indentline.lua
@@ -1,22 +1,22 @@
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 -- When confused try :help ibl.config
 return {
-    'lukas-reineke/indent-blankline.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-        exclude = {
-            filetypes = { 'markdown' },
-        },
-        scope = {
-            enabled = false,
-        },
-        indent = {
-            -- Easiest way to get chars to use
-            -- https://github.com/lukas-reineke/indent-blankline.nvim/blob/master/doc/indent_blankline.txt
-            char = '▏',
-        },
-    },
-    config = function()
-        vim.keymap.set('n', '<leader>i', '<cmd>IBLToggle<cr>', { silent = true, desc = 'Toggle Indentline display', })
-    end
+	"lukas-reineke/indent-blankline.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		exclude = {
+			filetypes = { "markdown" },
+		},
+		scope = {
+			enabled = false,
+		},
+		indent = {
+			-- Easiest way to get chars to use
+			-- https://github.com/lukas-reineke/indent-blankline.nvim/blob/master/doc/indent_blankline.txt
+			char = "▏",
+		},
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>ti", "<cmd>IBLToggle<cr>", { silent = true, desc = "Toggle Indentline" })
+	end,
 }
