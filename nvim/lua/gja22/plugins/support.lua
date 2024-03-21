6b49aa3a60169533a78ca987ce1dacc90a7f2077
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2024-03-21 16:27:02 -0700

    Autoformat

--- nvim/lua/gja22/plugins/support.lua
+++ nvim/lua/gja22/plugins/support.lua
@@ -1,19 +1,19 @@
 -- various plugins to support nvim tools
 return {
-    {
-        'folke/neodev.nvim',
-        config = function()
-            require('neodev').setup()
-        end
-    },
-    {
-        'j-hui/fidget.nvim',
-        tag = 'legacy',
-        config = function()
-            require('fidget').setup()
-        end
-    },
-    'nvim-lua/plenary.nvim',
-    'nvim-lua/popup.nvim',
-    'nvim-tree/nvim-web-devicons',
+	{
+		"folke/neodev.nvim",
+		config = function()
+			require("neodev").setup()
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		tag = "legacy",
+		config = function()
+			require("fidget").setup()
+		end,
+	},
+	"nvim-lua/plenary.nvim",
+	"nvim-lua/popup.nvim",
+	"nvim-tree/nvim-web-devicons",
 }
