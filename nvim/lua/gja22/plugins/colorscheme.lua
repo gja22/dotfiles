3fcc49e77df246dfcb6e676498e282973cb0788f
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2024-03-21 15:31:19 -0700

    Reformat switched spaces to tabs

--- nvim/lua/gja22/plugins/colorscheme.lua
+++ nvim/lua/gja22/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
-    -- 'folke/tokyonight.nvim',
-    -- 'lunarvim/darkplus.nvim',
-    -- 'gruvbox-community/gruvbox',
-    -- 'Tsuzat/NeoSolarized.nvim',
-    -- 'rose-pine/neovim',
-    -- 'navarasu/onedark.nvim',
-    {
-        'rebelot/kanagawa.nvim',
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme('kanagawa-wave')
-        end
-    }
+	-- 'folke/tokyonight.nvim',
+	-- 'lunarvim/darkplus.nvim',
+	-- 'gruvbox-community/gruvbox',
+	-- 'Tsuzat/NeoSolarized.nvim',
+	-- 'rose-pine/neovim',
+	-- 'navarasu/onedark.nvim',
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("kanagawa-wave")
+		end,
+	},
 }
