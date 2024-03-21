afc18ef571bf4cfe4f86ff2a5c9d6e261ce4089c
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2024-03-21 16:25:14 -0700

    Autoformat

--- nvim/lua/gja22/globals.lua
+++ nvim/lua/gja22/globals.lua
@@ -4,16 +4,16 @@
 -- by default, print will just display the
 -- memory address of a table
 P = function(v)
-    print(vim.inspect(v))
-    return v
+	print(vim.inspect(v))
+	return v
 end
 
 RELOAD = function(...)
-    return require("plenary.reload").reload_module(...)
+	return require("plenary.reload").reload_module(...)
 end
 
 -- Easy way to reload a lua function
 R = function(name)
-    RELOAD(name)
-    return require(name)
+	RELOAD(name)
+	return require(name)
 end
--- nvim/lua/gja22/init.lua
+++ nvim/lua/gja22/init.lua
@@ -1,4 +1,4 @@
-require('gja22.globals')
-require('gja22.options')
-require('gja22.keymaps')
-require('gja22.lazy')
+require("gja22.globals")
+require("gja22.options")
+require("gja22.keymaps")
+require("gja22.lazy")
