94da69e9210ddf627b93e825171f8d51fae7c460
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2024-03-21 16:41:03 -0700

    New plugins

---
+++ nvim/lua/gja22/plugins/capture.lua
@@ -0,0 +1,24 @@
+local nmap = function(keys, func, desc)
+	vim.keymap.set("n", keys, func, { desc = desc })
+end
+
+return {
+	"gja22/capture.nvim",
+	config = function()
+		nmap("<leader>zo", function()
+			require("capture").oneonone()
+		end, "Zettel 1-1")
+		nmap("<leader>zm", function()
+			require("capture").meeting()
+		end, "Zettel neeting")
+		nmap("<leader>zn", function()
+			require("capture").note()
+		end, "Zettel note")
+		nmap("<leader>zd", function()
+			require("capture").daily()
+		end, "Zettel daily")
+		nmap("<leader>zw", function()
+			require("capture").weekly()
+		end, "Zettel weekly")
+	end,
+}
