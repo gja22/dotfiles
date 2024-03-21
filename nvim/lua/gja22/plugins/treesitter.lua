4c0d54f74926e24c8f453e7025044782aebd44dd
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2024-03-21 16:00:10 -0700

    Reformat

--- nvim/lua/gja22/plugins/treesitter.lua
+++ nvim/lua/gja22/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = function()
-        require('nvim-treesitter.install').update({ with_sync = true })()
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	build = function()
+		require("nvim-treesitter.install").update({ with_sync = true })()
+	end,
 }
