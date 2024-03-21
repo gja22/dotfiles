0c96a1ba58720eccd4f1495c5646cf648e6cd0d2
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2024-03-21 15:41:39 -0700

    Autoformat and keymap tweak

--- nvim/lua/gja22/plugins/undotree.lua
+++ nvim/lua/gja22/plugins/undotree.lua
@@ -1,7 +1,7 @@
 -- https://github.com/mbbill/undotree
 return {
-    'mbbill/undotree',
-    config = function()
-        vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = '[U]ndotree Toggle' })
-    end
+	"mbbill/undotree",
+	config = function()
+		vim.keymap.set("n", "<leader>tu", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
+	end,
 }
