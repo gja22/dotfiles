f55c916d56bd5ff00d5a4b98860478e931087a23
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2024-03-21 15:33:17 -0700

    Minor keymap tweak

--- nvim/lua/gja22/plugins/harpoon.lua
+++ nvim/lua/gja22/plugins/harpoon.lua
@@ -5,8 +5,8 @@ return {
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
 
-		vim.keymap.set("n", "<leader>h", mark.add_file, { desc = "[H]arpoon file" })
-		vim.keymap.set("n", "<leader>l", ui.toggle_quick_menu, { desc = "[L]ist Harpoon files" })
+		vim.keymap.set("n", "<leader>h", mark.add_file, { desc = "Harpoon file" })
+		vim.keymap.set("n", "<leader>j", ui.toggle_quick_menu, { desc = "Jump to  Harpoon files" })
 		-- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, { desc = 'Harpoon file 1' })
 		-- vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end, { desc = 'Harpoon file 2' })
 		-- vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end, { desc = 'Harpoon file 3' })
