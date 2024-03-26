241d21554bc5c73c7dfa5474992df151b7168b60
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2024-03-26 07:11:59 -0700

    Finally configured Treesitter and actually got it working

--- nvim/lua/gja22/plugins/treesitter.lua
+++ nvim/lua/gja22/plugins/treesitter.lua
@@ -1,7 +1,76 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
-return {
+return { -- Highlight, edit, and navigate code
+	-- "nvim-treesitter/nvim-treesitter",
+	-- build = function()
+	-- 	require("nvim-treesitter.install").update({ with_sync = true })()
+	-- end,
 	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })()
+	build = ":TSUpdate",
+	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+	opts = {
+		ensure_installed = { "bash", "c", "go", "html", "lua", "markdown", "vim", "vimdoc" },
+		-- Autoinstall languages that are not installed
+		auto_install = true,
+		highlight = {
+			enable = true,
+			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+			--  If you are experiencing weird indenting issues, add the language to
+			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
+			-- additional_vim_regex_highlighting = { "ruby" },
+		},
+		-- indent = { enable = true, disable = { "ruby" } },
+		textobjects = {
+			select = {
+				enable = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					-- ["ac"] = "@class.outer",
+					-- ["ic"] = "@class.inner",
+				},
+			},
+			swap = {
+				enable = true,
+				swap_next = {
+					["<leader>a"] = "@parameter.inner",
+				},
+				swap_previous = {
+					["<leader>A"] = "@parameter.inner",
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true,
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
+		---@diagnostic disable-next-line: missing-fields
+		require("nvim-treesitter.configs").setup(opts)
+
+		-- There are additional nvim-treesitter modules that you can use to interact
+		-- with nvim-treesitter. You should go explore a few and see what interests you:
+		--
+		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	end,
 }
