8a7411cf1126427a21078f538796d0352bcc73b1
Geoffrey Apps <gja22@Geoffreys-MacBook-Pro.local>
2024-04-24 13:13:01 -0700

    Changed next/prev hunk keymap and added descriptions

--- nvim/lua/gja22/plugins/gitsigns.lua
+++ nvim/lua/gja22/plugins/gitsigns.lua
@@ -20,43 +20,43 @@ return {
 			end
 
 			-- Navigation
-			map("n", "]c", function()
+			map("n", "]h", function()
 				if vim.wo.diff then
-					return "]c"
+					return "]h"
 				end
 				vim.schedule(function()
 					gs.next_hunk()
 				end)
 				return "<Ignore>"
-			end, { expr = true })
+			end, { expr = true, desc = "Next Hunk" })
 
-			map("n", "[c", function()
+			map("n", "[h", function()
 				if vim.wo.diff then
-					return "[c"
+					return "[h"
 				end
 				vim.schedule(function()
 					gs.prev_hunk()
 				end)
 				return "<Ignore>"
-			end, { expr = true })
+			end, { expr = true, desc = "Prev Hunk" })
 
 			-- Actions
 			-- map('n', '<leader>hs', gs.stage_hunk)
-			map("n", "<leader>hr", gs.reset_hunk)
+			map("n", "<leader>hr", gs.reset_hunk, { desc = "Hunk Reset (Git)" })
 			-- map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
 			-- map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
 			-- map('n', '<leader>hS', gs.stage_buffer)
 			-- map('n', '<leader>hu', gs.undo_stage_hunk)
-			map("n", "<leader>hR", gs.reset_buffer)
-			map("n", "<leader>hp", gs.preview_hunk)
+			map("n", "<leader>hR", gs.reset_buffer, { desc = "Hunk Reset All (Git)" })
+			map("n", "<leader>hp", gs.preview_hunk, { desc = "Hunk Preview (Git)" })
 			map("n", "<leader>hb", function()
 				gs.blame_line({ full = true })
-			end)
+			end, { desc = "Hunk Blame (Git)" })
 			-- map('n', '<leader>tb', gs.toggle_current_line_blame)
-			map("n", "<leader>hd", gs.diffthis)
+			map("n", "<leader>hd", gs.diffthis, { desc = "Hunk Diff This (Git)" })
 			map("n", "<leader>hD", function()
 				gs.diffthis("~")
-			end)
+			end, { desc = "Hunk Diff File (Git)" })
 			-- map('n', '<leader>td', gs.toggle_deleted)
 
 			-- Text object
